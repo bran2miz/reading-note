dfff507d9c892e91f07eadc4acb5045a00ed9b03
brandonmizutani <bran2miz@gmail.com>
2021-08-18 19:42:17 -0700

    added a chart and the new function

--- app.js
+++ app.js
@@ -4,12 +4,13 @@
 let allProducts = [];
 // querySelector = method
 let myContainer = document.querySelector('section');
-let myButton = document.querySelector('section + div');
 let image1 = document.querySelector('section img:first-child');
 let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
 let clicks = 0;
 let clicksAllowed = 25;
+let indexArray = [];
+let uniqueNumberOfIndexes= 6;
 
 // constructor
 function Products(name, fileExtension = 'jpg') {
@@ -22,17 +23,25 @@ function Products(name, fileExtension = 'jpg') {
 function selectRandomProduct() {
   return Math.floor(Math.random() * allProducts.length);
 }
+
 function renderProducts() {
   // call the selectRandomProduct
-  let prod1 = selectRandomProduct();
-  let prod2 = selectRandomProduct();
-  let prod3 = selectRandomProduct();
+  while(indexArray.length < uniqueNumberOfIndexes) {
+    let randomNumber = selectRandomProduct();
+    if (!indexArray.includes(randomNumber)){
+      indexArray.push(randomNumber);
+    }
+  }
+
+  let prod1 = indexArray.shift();
+  let prod2 = indexArray.shift();
+  let prod3 = indexArray.shift();
   // push product values into array
   // google MDN array has value
-  while  (prod1 === prod2 || prod1 === prod3 || prod2 === prod3) {
-    prod2 = selectRandomProduct();
-    prod3 = selectRandomProduct();
-  }
+  // while  (prod1 === prod2 || prod1 === prod3 || prod2 === prod3) {
+  //   prod2 = selectRandomProduct();
+  //   prod3 = selectRandomProduct();
+  // }
   image1.src = allProducts[prod1].src;
   image2.src = allProducts[prod2].src;
   image3.src = allProducts[prod3].src;
@@ -42,6 +51,7 @@ function renderProducts() {
   allProducts[prod1].views++;
   allProducts[prod2].views++;
   allProducts[prod3].views++;
+
 }
 function handleProductClick(event) {
   if (event.target === myContainer) {
@@ -58,18 +68,20 @@ function handleProductClick(event) {
   }
   renderProducts();
   if (clicks === clicksAllowed) {
-    myButton.className = 'clicks-allowed';
+    
     myContainer.removeEventListener('click', handleProductClick);
+    renderChart();
   }
 }
-function renderResults() {
-  let ul = document.querySelector('ul');
-  for (let i = 0; i < allProducts.length; i++) {
-    let li = document.createElement('li')
-    li.textContent = `${allProducts[i].name} had ${allProducts[i].views} view and was clicked ${allProducts[i].clicks} times.`;
-    ul.appendChild(li);
-  }
-}
+// function renderResults() {
+//   let ul = document.querySelector('ul');
+//   for (let i = 0; i < allProducts.length; i++) {
+//     let li = document.createElement('li')
+//     li.textContent = `${allProducts[i].name} had ${allProducts[i].views} view and was clicked ${allProducts[i].clicks} times.`;
+//     ul.appendChild(li);
+//   }
+// }
+
 new Products ('bag');
 new Products ('banana');
 new Products ('bathroom');
@@ -93,5 +105,50 @@ new Products ('wine-glass');
 console.log(allProducts);
 renderProducts();
 
+function renderChart() {
+  let numClicks = [];
+  let numViews = [];
+  let numNames = [];
+  for (let i = 0; i < allProducts.length; i++) {
+    numNames.push(allProducts[i].name);
+    numClicks.push(allProducts[i].clicks);
+    numViews.push(allProducts[i].views);
+  }
+  //console.log(goatClicks);
+  let chartObject = {
+    type: 'bar',
+    data: {
+      labels: numNames,
+      datasets: [{
+        label: 'Number of Views',
+        data: numViews,
+        backgroundColor: '#5F9EA0',
+        borderColor: '#008B8B',
+        borderWidth: 1
+      },
+      {
+        label: 'Number of Clicks',
+        data: numClicks,
+        backgroundColor: '#2F4F4F',
+        borderColor: '#8FBC8F',
+        borderWidth: 1
+      }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  };
+  let ctx = document.getElementById('productChart').getContext('2d');
+  let myChart = new Chart(ctx, chartObject);
+}
+
+
+
 myContainer.addEventListener('click', handleProductClick);
-myButton.addEventListener('click', renderResults);
+// myButton.addEventListener('click', renderResults);
+
